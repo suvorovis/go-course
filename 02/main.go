cbf61c581f2454aca94d9c0573b455dce3abdb9d
suvorovis <suvorovis@yandex.ru>
2021-09-19 19:45:28 +0300

    02 type fix

--- 02/main.go
+++ 02/main.go
@@ -9,24 +9,24 @@ func main() {
 }
 
 func Unpack(s string) string {
-	var escape uint8
+	var escape bool
 	var symbol rune
 	var result []rune
 	for _, r := range s {
 		switch {
-		case r >= 'a' && r <= 'z' || escape == 1:
+		case r >= 'a' && r <= 'z' || escape:
 			if symbol != 0 {
 				result = append(result, symbol)
 			}
 			symbol = r
-			escape = 0
+			escape = false
 		case r >= '2' && r <= '9' && symbol != 0:
 			for i := 0; i < int(r-'0'); i++ {
 				result = append(result, symbol)
 			}
 			symbol = 0
 		case r == '\\':
-			escape = 1
+			escape = true
 		}
 	}
 	if symbol != 0 {
